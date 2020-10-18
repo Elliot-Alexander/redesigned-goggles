b2e10cbe15dc8a336a4efb55c00455dd2b97b226
SiJingZ <SiJingZ@users.noreply.github.com>
2020-10-18 10:58:00 +1000

    probably broken

--- src/socks/sockserv.js
+++ src/socks/sockserv.js
@@ -4,6 +4,31 @@ const options = {
   /* ... */
 };
 const io = require("socket.io")(server, options);
+const rcon = require("rcon")
+const { spawn } = require("child_process");
+
+const login = spawn("gcloud.cmd", [
+  "container",
+  "clusters",
+  "get-credentials",
+  "cluster-1",
+  "--zone",
+  "us-central1-c",
+  "--project",
+  "hackathon2020-292800",
+]);
+
+login.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
+
+login.stderr.on("data", (data) => {
+  console.error(data.toString());
+});
+
+login.on("exit", (code) => {
+  console.log(`Child exited with code ${code}`);
+});
 
 app.get("/", (req, res) => {
   res.send(
@@ -11,55 +36,84 @@ app.get("/", (req, res) => {
   );
 });
 
-processes = [];
-users = [];
+rooms = [];
+users = {};
+pods = [];
+active = {};
 
 io.on("connection", (socket) => {
   socket.on("lfg", () => {
-    socket.emit("lfg", processes);
+    socket.emit("lfg", rooms);
     console.log("joined lfg");
   });
   socket.on("register_user", (username, room_id) => {
     socket.emit(
       "roomCheck",
-      !(users.includes(username) || processes.includes(room_id)) &&
+      !users[room_id].includes(username) &&
         !(
           username === undefined ||
           username === "" ||
-          room_id === undefined || room_id === ""
+          room_id === undefined ||
+          room_id === ""
         )
     );
-    console.log(processes);
-    console.log(users);
     if (
       !(
         username === undefined ||
         username === "" ||
-        room_id === undefined || room_id === ""
+        room_id === undefined ||
+        room_id === ""
       )
     ) {
-      users.push(username);
-      processes.push(room_id);
-    }
-    console.log("check");
-    socket.on("command"+room_id, (command) => {
-      "kubectl exec -i POD_NAME COMMAND"
-    })
-    socket.join("command"+room_id);
+      if (!rooms.includes(room_id)) {
+        rooms.push(room_id);
+        socket.on("command" + room_id, (command) => {
+          let conn = new rcon(active[room_id][0], "25575", "minecraft")
+          conn.on('auth', function() {
+            console.log("Authed!");
+            conn.send(command)
+        }).on('response', function(str) {
+            console.log("Got response: " + str);
+            socket.emit("console", str)
+        }).on('end', function() {
+            console.log("Socket closed!");
+            process.exit();
+        });
+          "kubectl exec -i POD_NAME COMMAND";
+        });
+        socket.to("lfg").broadcast.emit("new_room", room_id);
+        const scaleUpSet = spawn("kubectl", ["scale", "sts", "minecraft-server-set", "--replicas=" + rooms.length])
+        const  getPods = spawn("kubectl", ["get", "pods","-o", "json"])
+
+        let pod_name = JSON.parse(getPods).items[-1].metadata.name;
+        const  exposePod = spawn("kubectl", ["expose", "pod",pod_name, "type=LoadBalancer"])
+        const  getServices = spawn("kubectl", ["get", "svc","-o", "json"])
+        let ip = JSON.parse(getServices).items[-1].status.loadBalancer.ingress[0].ip
+        active[room_id] = [ip, pod_name]
+        socket.to(room_id).emit("ip", ip)
+        users[room_id] = [];
+      }
+      users[room_id].push(username);
+      socket.emit("users", users[room_id]);
+    }    
+    socket.join(room_id)
+    socket.to(room_id).broadcast.emit("new_user", username)
+    socket.emit("ip", active[room_id][0])
     socket.leave("lfg");
-    socket.to("lfg").broadcast.emit("new_room", room_id)
   });
+
   socket.on("generate_id", () => {
     let code = Math.random()
       .toString(36)
       .substring(7);
-    while (processes.includes(code)) {
+    while (rooms.includes(code)) {
       code = Math.random()
         .toString(36)
         .substring(7);
     }
     socket.emit("generate_id", code);
   });
+
 
   socket.join("lfg");
 });
